6afa03875f090e2c4fc9f9e1c7928d54e8b5714d
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-02-24 13:16:08 +0800

    20170223 Revisied LCD+Temp OK

--- Doghouse.ino
+++ Doghouse.ino
@@ -1,41 +1,55 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
-LiquidCrystal lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, HIGH); //LCD object 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);    //LCD object 
 const int Led=13;
 const int buttonpin=3;
-int ntc=A0;
+int ntc=A0; //ntc temp value sensor.
 int val;
 int transistor=8;
-void lcddisplay(double temp,double weight1)
+void lcddisplay(double temp,double weight)
 {
-   lcd.setCursor(0,0);
-   lcd.print("Temp:");
-   lcd.print(temp,DEC);
+   int temp_int;
+   temp_int=(int)temp;
+   
    if(temp<25)
    {
-    lcd.print("Cold");
+    lcd.setCursor(0,0);
+    lcd.print("Temp:");
+    lcd.print(temp_int);
+    lcd.setCursor(9,0); //Col 12 Row 0
+    lcd.print("Cold>_<");
    }
-   if(weight<100) //100 grams of feed, recharge
+   else
+   {
+    lcd.setCursor(0,0);
+    lcd.print("Temp:");
+    lcd.print(temp_int);
+    lcd.setCursor(9,0); //Col 12 Row 0
+    lcd.print(" 
+    
+    Fine:)");
+   }
+   /*if(weight<100) //100 grams of feed, recharge
    {
     lcd.setCursor(0,1);
     lcd.print("Refill food!");
-   }
+   }*/
    
 }
 
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); //serial IO speed
   pinMode(Led,OUTPUT);
   pinMode(buttonpin,INPUT);
-  Serial.print("1111");
+  //LCD required procedure
 
-  lcd.begin(16, 2);  //1604
-  lcd.setCursor(0,0);
+  lcd.begin(16, 2);  
 }
 
-double Thermistor(int RawADC) {
+double Thermistor(int RawADC) //thermal sensor for KY 038-like module
+{
   double Temp;
   Temp = log(10000.0*((1024.0/RawADC-1))); 
   Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
@@ -44,20 +58,12 @@ double Thermistor(int RawADC) {
    return 43-Temp;
 }
 
-void loop()
+void loop() //system loop
 {
-  val=analogRead(ntc);
-  Serial.print(val);
-  Serial.print('\n');
-  /*if(val==HIGH)
-  {
-    digitalWrite(Led,HIGH);
-  }
-  else
-  {
-    digitalWrite(Led,LOW);
-  }*/
+  val=analogRead(ntc); //get specified temp data by analog read at the specified analog port
+  
   double temp =  Thermistor(val);
+  delay(1000);
   Serial.print("Temperature big sensor is = ");
   Serial.print(temp);
   Serial.println(" Celsius, ");
@@ -69,7 +75,7 @@ void loop()
   {
     digitalWrite(transistor,LOW);
   }
-  lcddisplay(temp);
+  lcddisplay(temp,temp);
 }
 
 
